4f4b0e58742240fb0d11d04307b254307c811a5e
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-12 15:40:18 -0700

    updating putDb

---
+++ client/src/js/database.js
@@ -0,0 +1,34 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, value) => {
+  console.error('putDb not implemented');
+  // Connect to the DB
+  const jateDb = await openDB("jate", 1);
+  // Creating a new transaction
+  const tx = jateDb.transaction("jate", "readwrite");
+  // Open object score
+  const objStore = tx.objectStore("jate");
+  // Pass in the correct data
+  const request = objStore.put({ id: id, value: value });
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
